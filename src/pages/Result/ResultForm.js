3c78c8a7e5515d55c3555811de9e12be635c564a
zeeshan191 <80030867+zeeshan191@users.noreply.github.com>
2021-04-25 20:53:30 +0530

    edusoft

---
+++ src/pages/Result/ResultForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResultForm() {
+    return (
+        <div>
+            lol nai
+        </div>
+    )
+}
+
+export default ResultForm;
---
+++ src/pages/Result/ResultReport.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResultReport() {
+    return (
+        <div>
+            ggggggggggggggggggggggggggggggg
+        </div>
+    )
+}
+
+export default ResultReport
